f1cd4aa4959de3bd6ddeadc5a7cd211e717b994d
ErickOTC <80046344+ErickOTC@users.noreply.github.com>
2024-02-24 17:05:17 -0300

    aulas 01 e 02 conculidas

---
+++ oneBitCode_JS/001-tiposDeDados/js/script.js
@@ -0,0 +1,33 @@
+// Tipos de dados
+
+    // number
+    let num01 = 195654423236;
+    let num02 = 1.95654423236;
+
+    // bigint
+    let big = 195654423236565465489454546546464;
+    let bint = 195654423236565465489454546546464n;
+
+    // string
+    let txt01 = "Erick";
+    let txt02 = 'Erick';
+    let txt03 = "12";
+    let txt04 = "1.2";
+
+        // length = comprimento
+    
+    // boolean
+        let verdade = true; // palavra reservada valor igual a 1 ou verdadeiro
+        let mentira = false; // palavra reservada valor igual a 0 ou falso
+
+    // null
+    let vazio = null; // recebeu um valor vazio {}
+
+    let indefinido = undefined; // até agora não foi adicionado nada e por padrão toda variável sem valor, recebe undefined como padrão js
+
+    // extra
+        // objetos
+            // conjunto de várias informações
+
+        // funções
+            // tarefas a serem realizadas ordenadamente
---
+++ oneBitCode_JS/002-variaveis/js/script.js
@@ -0,0 +1,13 @@
+var idade; // variável
+let nome; // variável
+const sobrenome = teodoro; // constante, não pode ser modificado
+
+idade = 21
+nome = "Erick"
+
+//Cases
+    // camelCase
+        // let nomeDeUsuario
+
+    // snake_case
+        // let nome_de_usuario 
