071964a3ce1deded715dff95d70cfde9a6520167
ErickOTC <80046344+ErickOTC@users.noreply.github.com>
2024-03-14 00:05:18 -0300

    up

---
+++ oneBitCode_JS/003-operadoresExpressoes/js/script.js
@@ -0,0 +1,46 @@
+//operadores
+    // Variáveis
+        let num01 = 10;
+        let num02 = 3;
+
+    // adição
+        let adicao = num01 + num02;
+
+    //subtração
+        let subtracao = num01 - num02;
+
+    //multiplicação
+        let multiplicacao = num01 * num02;
+
+    //divisão
+        //divisão com casas decimais
+            let divisao = num01 / num02;
+
+        //resto
+            let resto = num01 % num02;
+
+
+    //agrupamento ()
+        let agrupamento = 3+(3*5);
+
+    //atribuição
+        //opção 1
+            let atribuicao01 = 3;
+            atribuicao01 = atribuicao01 + 3
+
+        // opção 2
+            let atribuicao02 = 4;
+            atribuicao02 += 4
+
+    //encadeamento
+        console.error('ERRO')
+
+    //inclementar
+        let inclementar = 4;
+            inclementar ++
+            ++ inclementar
+
+    //declementar
+        let declementar = 4;
+            declementar --
+            -- declementar
